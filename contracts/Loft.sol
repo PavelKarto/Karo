04e6b4967547031775892e3db269d14a8734d653
PavelKarto <pavelkartofan@gmail.com>
2025-12-11 13:34:53 +0100

    2+

--- contracts/Loft.sol
+++ contracts/Loft.sol
@@ -6,5 +6,5 @@ pragma solidity ^0.8.27;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Loft is ERC20 {
-    constructor() ERC20("Loft", "MTK") {}
+    constructor() ERC20("Loft", "OPO") {}
 }
